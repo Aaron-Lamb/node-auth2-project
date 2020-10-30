a293b14beaa052415e6dd5abfa037cbccb983746
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-10-29 19:02:58 -0700

    Basic server ready, knexfil;e set up, and initial migration created.

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const cors = require('cors');
+const welcomeRouter = require('./welcome/welcomeRouter');
+
+const server = express();
+const port = process.env.PORT || 4000;
+
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+server.use(cookieParser());
+server.use(welcomeRouter);
+
+server.use((err, req, res, next) => {
+    console.log(err);
+    return res.status(500).json({
+        errorMessage: "A server error has occurred"
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
